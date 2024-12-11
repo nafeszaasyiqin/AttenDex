1c84f07314d4eb5d1543bccf3b11434770934b0e
Nur Nafesza Asyiqin binti Ismail <nafesza@coreconsulting.asia>
2024-12-11 22:38:59 +0800

    Initial Spring Boot setup

---
+++ src/main/java/com/example/Attendex/AttendexApplication.java
@@ -0,0 +1,14 @@
+package com.example.Attendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendexApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/Attendex/AttendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Attendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
